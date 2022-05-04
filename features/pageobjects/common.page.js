bb985618d927d9bc148406fec636a0d245a94247
Nishant Chopra <nishantchopra14@gmail.com>
2022-05-05 02:18:13 +0530

    first commit

---
+++ features/pageobjects/common.page.js
@@ -0,0 +1,19 @@
+class commonPage {
+    openHomePage() {
+      browser.url("http://automationpractice.com/index.php");
+      console.log("Navigating to Url 'http://automationpractice.com/index.php'");
+    }
+  
+    vaerifyPageHeading = async (title) => {
+      await browser.waitUntil(
+        async () => (await $(".page-heading").getText()) === title,
+        {
+          timeout: 10000,
+          timeoutMsg: "expected text is different after 10s",
+        }
+      );
+      const headingTitle = await $(".page-heading");
+      expect(await headingTitle.getText()).toEqual(title);
+    };
+  }
+  export default new commonPage();
---
+++ features/step-definitions/auth.step.js
@@ -0,0 +1,52 @@
+import { Given, When, Then } from "@cucumber/cucumber";
+import faker from "faker";
+import authPage from "../pageobjects/auth.page";
+import HomePage from "../pageobjects/home.page";
+import allureReporter from '@wdio/allure-reporter'
+import utils from '../utils/Utils'
+
+
+Then(/^create an account with random username$/, async () => {
+  console.log("account creation ");
+  allureReporter.addStep("create an account with random username");
+  const randomStr = Math.random().toString(36).substr(2, 5);
+  const emailId = `myemail_${randomStr}@gmail.com`;
+  allureReporter.addSeverity("critical")
+  const addressObj = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    company: faker.company.companyName(),
+    address1: faker.address.streetAddress(),
+    address2: faker.random.alphaNumeric(5),
+  };
+
+  utils.dynamicData.email = emailId;
+  utils.dynamicData.address = addressObj;
+  await authPage.createAccount(emailId, addressObj);
+});
+
+Given(/^I am on the Sign In Page$/, async () => {
+
+  if (await HomePage.btnSignOut.isExisting()) {
+    await authPage.signOut();
+  }
+
+  await expect(HomePage.btnSignIn).toBeDisplayed();
+  await expect(HomePage.img_Logo).toBeDisplayed();
+  await expect(HomePage.link_Contact).toBeDisplayed();
+
+  await HomePage.navigateToLoginPage();
+});
+
+Given(/^Login using newly created (dynamic|static) credentials$/, async (credentialType) => {
+
+  let email = "";
+  if (credentialType == "dynamic") {
+    email = utils.dynamicData.email;
+    console.log("utils.dynamicData.email " + utils.dynamicData.email);
+  } else {
+    email = utils.staticData.email;
+  }
+  await authPage.signIn(email);
+
+});
